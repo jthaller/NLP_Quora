d1d9be359decfc16e1cf44c48862937511c263f6
Jeremy Thaller <jeremy@thallerfamily.com>
2020-02-09 22:08:45 +0100

    finished vocab embeddings

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # NLP_Quora
-Repository for Kaggle Quora Competition, using deep learning for natural language processing. 
+Repository for Kaggle Quora Competition, using deep learning for natural language processing.
 
 ## Motivation
-Before learning PyText and analyzing my downloaded facebook messages, first I some more guided practice. I will mostly be working on kaggle, but likely will truncate unecessary steps and create a more experimental and person project of my own here. I might end up using google colab for its unlimited gpu access. Even though I'm on intel's gen 10 ice lake graphics, tensorflow still resorts to only cpu usage, unlike kaggle or colab.
+Before learning PyText and analyzing my downloaded facebook messages, first I some more guided practice. I will mostly be working on kaggle, but likely will truncate unecessary steps and create a more experimental and person project of my own here. I might end up using google colab for its unlimited gpu access. Even though I'm on intel's gen 10 ice lake graphics, tensorflow still resorts to only cpu usage, unlike kaggle or colab. Additionally, the training set provided includes 1.3 million examples. It seems like it'd be quite a strain on my computer to try and compute this stuff. I wonder if I still have access to the TUM computing cluster...
 
 ## Kaggle Competition Prompt
 "An existential problem for any major website today is how to handle toxic and divisive content. Quora wants to tackle this problem head-on to keep their platform a place where users can feel safe sharing their knowledge with the world.
@@ -12,5 +12,3 @@ Quora is a platform that empowers people to learn from each other. On Quora, peo
 In this competition, Kagglers will develop models that identify and flag insincere questions. To date, Quora has employed both machine learning and manual review to address this problem. With your help, they can develop more scalable methods to detect toxic and misleading content.
 
 Here's your chance to combat online trolls at scale. Help Quora uphold their policy of “Be Nice, Be Respectful” and continue to be a place for sharing and growing the world’s knowledge."
-
-
--- quora.py
+++ quora.py
@@ -9,8 +9,10 @@ from torchtext import data
 import spacy
 from tqdm import tqdm_notebook, tnrange
 from tqdm.auto import tqdm
-
+from tqdm import tqdm
+# tqdm.pandas()
 tqdm.pandas(desc='Progress')
+
 from collections import Counter
 from textblob import TextBlob
 from nltk import word_tokenize
@@ -25,7 +27,7 @@ from torchtext.data import Example
 from sklearn.metrics import f1_score
 import torchtext
 import os
-
+from sklearn.preprocessing import StandardScaler
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
@@ -57,12 +59,270 @@ batch_size = 256 # how many samples to process at once
 n_epochs = 5 # how many times to iterate over all samples
 n_splits = 5 # Number of K-fold Splits
 
+# df_train = pd.read_csv("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\train.csv")
+# df_test = pd.read_csv("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\test.csv")
+# df = pd.concat([df_train ,df_test],sort=True)
 
-df_train = pd.read_csv("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\train.csv")
-df_test = pd.read_csv("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\test.csv")
-df = pd.concat([df_train ,df_test],sort=True)
+# preprocessing --------------------------------------------------------------------------------------
+## Methodss to build vocab and deal with exceptions --------------------------------------------------
+def build_vocab(texts):
+    sentences = texts.apply(lambda x: x.split()).values
+    vocab = {}
+    for sentence in sentences:
+        for word in sentence:
+            try:
+                vocab[word] += 1
+            except KeyError:
+                vocab[word] = 1
+    return vocab
 
-df_train.head()
-df_test.head()
-df_train.shape
-df_test.shape
+def known_contractions(embed):
+    known = []
+    for contract in contraction_mapping:
+        if contract in embed:
+            known.append(contract)
+    return known
+
+def clean_contractions(text, mapping):
+    specials = ["’", "‘", "´", "`"]
+    for s in specials:
+        text = text.replace(s, "'")
+    text = ' '.join([mapping[t] if t in mapping else t for t in text.split(" ")])
+    return text
+
+def correct_spelling(x, dic):
+    for word in dic.keys():
+        x = x.replace(word, dic[word])
+    return x
+
+def unknown_punct(embed, punct):
+    unknown = ''
+    for p in punct:
+        if p not in embed:
+            unknown += p
+            unknown += ' '
+    return unknown
+
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+def clean_special_chars(text, punct, mapping):
+    for p in mapping:
+        text = text.replace(p, mapping[p])
+
+    for p in punct:
+        text = text.replace(p, f' {p} ')
+
+    specials = {'\u200b': ' ', '…': ' ... ', '\ufeff': '', 'करना': '', 'है': ''}  # the fuck?
+    for s in specials:
+        text = text.replace(s, specials[s])
+    return text
+
+def add_lower(embedding, vocab):
+    count = 0
+    for word in vocab:
+        if word in embedding and word.lower() not in embedding:
+            embedding[word.lower()] = embedding[word]
+            count += 1
+    print(f"Added {count} words to embedding")
+
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…',
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─',
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞',
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+
+def clean_text(x):
+    x = str(x)
+    for punct in puncts:
+        x = x.replace(punct, f' {punct} ')
+    return x
+
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+mispell_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have", 'colour': 'color', 'centre': 'center', 'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling', 'theatre': 'theater', 'cancelled': 'canceled', 'labour': 'labor', 'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora', 'sallary': 'salary', 'Whta': 'What', 'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are', 'howcan': 'how can', 'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best', 'howdoes': 'how does', 'mastrubation': 'masturbation', 'mastrubate': 'masturbate', "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum', 'narcissit': 'narcissist', 'bigdata': 'big data', '2k17': '2017', '2k18': '2018', 'qouta': 'quota', 'exboyfriend': 'ex boyfriend', 'airhostess': 'air hostess', "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization', 'demonitization': 'demonetization', 'demonetisation': 'demonetization'}
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+    return mispellings_re.sub(replace, text)
+
+# Feaure Engineering -----------------------------------------------------------------
+def add_features(df):
+    df['question_text'] = df['question_text'].progress_apply(lambda x:str(x))  #makes a df that's just the questions
+    df['total_length'] = df['question_text'].progress_apply(len) #length of df
+    df['capitals'] = df['question_text'].progress_apply(lambda comment: sum(1 for c in comment if c.isupper())) # number of capital letters in question
+    df['caps_vs_length'] = df.progress_apply(lambda row: float(row['capitals'])/float(row['total_length']), axis=1) # density of capital letters
+    df['num_words'] = df.question_text.str.count('\S+') # num words in question
+    df['num_unique_words'] = df['question_text'].progress_apply(lambda comment: len(set(w for w in comment.split()))) # num unique words in question
+    df['words_vs_unique'] = df['num_unique_words'] / df['num_words']    # unique words density
+    return df
+
+# Inithialize tokenizer, read text, apply functions above (preprocessing and feature engineering)
+def load_and_prec():
+    train_df = pd.read_csv("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\train.csv")
+    test_df = pd.read_csv("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\test.csv")
+    print("Train shape : ",train_df.shape)
+    print("Test shape : ",test_df.shape)
+
+    # lower
+    train_df["question_text"] = train_df["question_text"].apply(lambda x: x.lower())
+    test_df["question_text"] = test_df["question_text"].apply(lambda x: x.lower())
+
+    # Clean the text
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_text(x))
+    test_df["question_text"] = test_df["question_text"].apply(lambda x: clean_text(x))
+
+    # Clean numbers
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_numbers(x))
+    test_df["question_text"] = test_df["question_text"].apply(lambda x: clean_numbers(x))
+
+    # Clean speelings
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: replace_typical_misspell(x))
+    test_df["question_text"] = test_df["question_text"].apply(lambda x: replace_typical_misspell(x))
+
+    ## fill up the missing values
+    train_X = train_df["question_text"].fillna("_##_").values
+    test_X = test_df["question_text"].fillna("_##_").values
+
+
+
+    ###################### Add Features ###############################
+
+    train = add_features(train_df)
+    test = add_features(test_df)
+
+    features = train[['caps_vs_length', 'words_vs_unique']].fillna(0)
+    test_features = test[['caps_vs_length', 'words_vs_unique']].fillna(0)
+
+    ss = StandardScaler()
+    ss.fit(np.vstack((features, test_features)))
+    features = ss.transform(features)
+    test_features = ss.transform(test_features)
+    ###########################################################################
+
+    ## Tokenize the sentences, as in introductory example
+    tokenizer = Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(train_X))
+    train_X = tokenizer.texts_to_sequences(train_X)
+    test_X = tokenizer.texts_to_sequences(test_X)
+
+    ## Pad the sentences
+    train_X = pad_sequences(train_X, maxlen=maxlen)
+    test_X = pad_sequences(test_X, maxlen=maxlen)
+
+    ## Get the target values
+    train_y = train_df['target'].values
+
+
+    #shuffling the data
+    np.random.seed(123)
+    trn_idx = np.random.permutation(len(train_X))
+
+    train_X = train_X[trn_idx]
+    train_y = train_y[trn_idx]
+    features = features[trn_idx]
+
+    return train_X, test_X, train_y, features, test_features, tokenizer.word_index
+
+# save some memory so we don't have to do it all again ----------------------------------
+np.save("x_train",x_train)
+np.save("x_test",x_test)
+np.save("y_train",y_train)
+
+np.save("features",features)
+np.save("test_features",test_features)
+np.save("word_index.npy",word_index)
+# load the saved processed data ---------------------------------------------------------
+x_train = np.load("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\x_train.npy")
+x_test = np.load("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\x_test.npy")
+y_train = np.load("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\y_train.npy")
+features = np.load("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\features.npy")
+test_features = np.load("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\test_features.npy")
+word_index = np.load("C:\\Users\\jerem\\OneDrive\\Documents\\Python\\NLP_Quora\\word_index.npy").item()
+
+
+# To save time, people use pre-trained embeddings glove, fasttext and paragram
+# Here glove is used for some twitter project: https://www.kaggle.com/christofhenkel/how-to-preprocessing-when-using-embeddings
+# Then build an embedded matrix
+with open("../input/glove-wiki-twitter2550/glove.twitter.27B.50d.txt") as f:
+    lines = f.readlines()
+lines = [line.rstrip().split() for line in lines]
+
+print(len(lines))         # number of words (aka vocabulary size)
+print(len(lines[0]))      # length of a line
+print(lines[99][0])       # word 99
+print(lines[99][1:])      # vector representation of word 99
+print(len(lines[99][1:])) # dimensionality of word 99
+
+# functions which take the dictionary and return the matrix of words embedded in that particular settting.
+def load_glove(word_index):
+    EMBEDDING_FILE = '../input/quora-insincere-questions-classification/embeddings/glove.840B.300d/glove.840B.300d.txt'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE))
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean,emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    # word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+def load_fasttext(word_index):
+    EMBEDDING_FILE = '../input/quora-insincere-questions-classification/embeddings/wiki-news-300d-1M/wiki-news-300d-1M.vec'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE) if len(o)>100)
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean,emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    # word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+def load_para(word_index):
+    EMBEDDING_FILE = '../input/quora-insincere-questions-classification/embeddings/paragram_300_sl999/paragram_300_sl999.txt'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE, encoding="utf8", errors='ignore') if len(o)>100)
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean,emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    # word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
